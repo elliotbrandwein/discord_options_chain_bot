b63136203dc2df6ef88d2eb24e5a6f2def5b2b3e
Jacob <jl@jacoblinder.net>
2020-12-03 19:56:26 -0500

    youll be pleased

--- yahoo.py
+++ yahoo.py
@@ -3,29 +3,47 @@ from yahoo_fin import options as opt
 from yahoo_fin import stock_info as si
 import datetime as dt
 import pandas as pd
-print("please give me a ticka")
+
+def ticka_price(ticka):
+    return si.get_live_price(ticka)
+
+def get_middle(chain, ticka):
+    stock_price = ticka_price(ticka)
+    prev_dif = float("Inf") # set this way to make sure it gets past the first iteration
+    for index,row in chain.iterrows():
+        dif = abs(stock_price - row['Strike'])
+        if dif < prev_dif:
+            prev_dif = dif
+            continue
+        else:
+            return index
+    return -1 # throw an error here, add later
+
+def friday():
+    today = dt.date.today()
+    return today + dt.timedelta((4 - today.weekday()) % 7)
+
+def calls_chain(ticka):
+    calls = opt.get_calls(ticka,friday()) 
+    calls['profit'] = calls['Last Price'] / calls['Strike']
+    return calls
+
+def puts_chain(ticka):
+    puts = opt.get_puts(ticka,friday())
+    puts['profit'] = puts['Last Price'] / puts['Strike']
+    return puts
+
+print("please give me a ticka") # will replace with input from bot
 ticka = input()
-today = dt.date.today()
-soonest_friday = today + dt.timedelta((4 - today.weekday()) % 7)
-stock_price = si.get_live_price(ticka)
-calls = opt.get_calls(ticka,soonest_friday)
-puts = opt.get_puts(ticka,soonest_friday)
-calls['profit'] = calls['Last Price'] / calls['Strike']
 
-middle = 0 #to save for later
-prev_dif = float("Inf") # set this way to make sure it gets past the first iteration
-for index,row in calls.iterrows():
-    dif = abs(stock_price - row['Strike'])
-    if dif < prev_dif:
-        prev_dif = dif
-        continue
-    else:
-        middle = index
-        break
+calls = calls_chain(ticka)
+puts = puts_chain(ticka)
 
-return_calls = calls.iloc[middle-5:middle+5]
+call_middle = get_middle(calls, ticka)
+put_middle = get_middle(puts, ticka)
+
+return_calls = calls.iloc[call_middle-5:call_middle+5]
 print(return_calls)
 
-# use the same middle for puts
-return_puts = puts.iloc[middle-5:middle+5]
+return_puts = puts.iloc[put_middle-5:put_middle+5]
 print(return_puts)
