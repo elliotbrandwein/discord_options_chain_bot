b8e7fe807567d35d550d1b1a4e753df53825c2d9
Jacob <jl@jacoblinder.net>
2020-12-03 19:41:05 -0500

    puts and calls work how you want

--- yahoo.py
+++ yahoo.py
@@ -2,6 +2,7 @@
 from yahoo_fin import options as opt
 from yahoo_fin import stock_info as si
 import datetime as dt
+import pandas as pd
 print("please give me a ticka")
 ticka = input()
 today = dt.date.today()
@@ -10,4 +11,21 @@ stock_price = si.get_live_price(ticka)
 calls = opt.get_calls(ticka,soonest_friday)
 puts = opt.get_puts(ticka,soonest_friday)
 calls['profit'] = calls['Last Price'] / calls['Strike']
-print(calls)
+
+middle = 0 #to save for later
+prev_dif = float("Inf") # set this way to make sure it gets past the first iteration
+for index,row in calls.iterrows():
+    dif = abs(stock_price - row['Strike'])
+    if dif < prev_dif:
+        prev_dif = dif
+        continue
+    else:
+        middle = index
+        break
+
+return_calls = calls.iloc[middle-5:middle+5]
+print(return_calls)
+
+# use the same middle for puts
+return_puts = puts.iloc[middle-5:middle+5]
+print(return_puts)
