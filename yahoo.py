04c17be31f2a836c1234d65ada55c7e217f61f22
Elliot <elliot@pop-os.localdomain>
2020-12-06 10:19:22 -0500

    added bands

--- yahoo.py
+++ yahoo.py
@@ -4,11 +4,14 @@ from yahoo_fin import stock_info as si
 import datetime as dt
 import pandas as pd
 from prettytable import PrettyTable
+import datetime
+import math
+price_cache = {}
+
 def percentager(the_number):
     the_number = round(the_number,4)
     return "{:.2%}".format(the_number)
 
-price_cache = {}
 def ticka_price(ticka):
     price = si.get_live_price(ticka)
     price_cache[ticka] = price
@@ -62,13 +65,34 @@ def return_calls(ticka):
     calls['profit'] = calls['profit'].apply(percentager)
     return calls.iloc[call_middle-5:call_middle+5]
 
+def get_band(ticka, band_age=None):
+    if(band_age == None):
+        band_age = 30
+    band_age *= 2
+    #to get the start's rolling avg, we need to start at the age before it
+    start_date = datetime.datetime.now() - datetime.timedelta(band_age)
+    data = si.get_data(ticka,start_date=start_date)
+    # we only need date  and adjusted price
+    stockprices = data.drop(columns=['open', 'high', 'low', 'close', 'volume', 'ticker'])
+    #we calc the avg from the half way point, which should be roughly the first time we can calc the band
+    start_rolling = math.floor(len(stockprices) / 2)
+    stockprices['MA'] = stockprices['adjclose'].rolling(window=start_rolling).mean()
+    stockprices['STD'] = stockprices['adjclose'].rolling(window=start_rolling).std() 
+    stockprices['Upper'] = stockprices['MA'] + (stockprices['STD'] * 2)
+    stockprices['Lower'] = stockprices['MA'] - (stockprices['STD'] * 2)
+    #filter out all the NaN's from the bands we didnt cal
+    stockprices = stockprices.query('MA == MA')
+    return stockprices
+    
 if __name__ == '__main__':
     print("please give me a ticka") # will replace with input from bot
     ticka = input()
     print("call or put")
     option = input()
     option = option.lower()
-    if option == "call":
+    if option == "band":
+        print(get_band(ticka))
+    elif option == "call":
         ascii_table = PrettyTable()
         data = return_calls(ticka)
         if len(data) == 0:
@@ -83,7 +107,6 @@ if __name__ == '__main__':
             print(ascii_table.get_string())
 
     elif option == "put":
-        # try:
             ascii_table = PrettyTable()
             data = return_puts(ticka)
             if len(data) == 0:
@@ -96,7 +119,5 @@ if __name__ == '__main__':
                         ascii_table.add_row(["TICKA:",ticka.upper(),"-","-","CURRENT PRICE:", round(price_cache[ticka],4)])
                     ascii_table.add_row(data.iloc[i])
                 print(ascii_table.get_string())
-        # except:
-        #     print("ERROR")
     else:
         print("you're an idiot")
