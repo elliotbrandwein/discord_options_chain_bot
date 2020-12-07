fd8e654f5207395a5927af6d89b8061757641e1c
Jacob <jl@jacoblinder.net>
2020-12-06 23:45:57 -0500

    mostly worksish

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import yahoo #our other file
 from dotenv import load_dotenv
 import os
 from prettytable import PrettyTable
+import pandas as pd
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,9 +17,17 @@ bot = commands.Bot(command_prefix='>') #change character here
 # then query = " ".join(args[:]) >>> query = query.strip()
 
 
+def danny_divito(rum_ham, ticka):
+    ascii_table = PrettyTable()
+    ascii_table.field_names = rum_ham.columns
+    for i in range(len(rum_ham.index)):
+        if i == 4:
+            ascii_table.add_row(["TICKA:",ticka.upper(),"-","-","PRICE:", round(yahoo.price_cache[ticka],4)])
+        ascii_table.add_row(rum_ham.iloc[i])
+    return ascii_table
+
 @bot.command(name='puts')
 async def put_getter(ctx, ticka):
-    ascii_table = PrettyTable()
     data = ""
     try:
         data = yahoo.return_puts(ticka)
@@ -26,11 +35,7 @@ async def put_getter(ctx, ticka):
             await ctx.send(f"```\nThere was no weekly options chain for the ticka {ticka}\n```")
         else:
             data = data.drop(columns=['Contract Name', 'Last Trade Date', 'Change', 'Implied Volatility', '% Change', 'Open Interest'])
-            ascii_table.field_names = data.columns
-            for i in range(len(data.index)):
-                if i == 4:
-                    ascii_table.add_row(["TICKA:",ticka.upper(),"-","-","PRICE:", round(yahoo.price_cache[ticka],4)])
-                ascii_table.add_row(data.iloc[i])
+            ascii_table = danny_divito(data, ticka)
             await ctx.send(f"```\n{ascii_table.get_string()}\n```")
     except:
       await ctx.send("There was an error please try again")
@@ -38,7 +43,6 @@ async def put_getter(ctx, ticka):
 
 @bot.command(name='calls')
 async def call_getter(ctx, ticka):
-    ascii_table = PrettyTable()
     data = ""
     try:
         data = yahoo.return_calls(ticka)
@@ -46,11 +50,7 @@ async def call_getter(ctx, ticka):
             await ctx.send(f"```\nThere was no weekly options chain for the ticka {ticka}\n```")
         else:
             data = data.drop(columns=['Contract Name', 'Last Trade Date', 'Change', 'Implied Volatility', '% Change', 'Open Interest'])
-            ascii_table.field_names = data.columns
-            for i in range(len(data.index)):
-                if i == 4:
-                    ascii_table.add_row(["TICKA:",ticka.upper(),"-","-","PRICE:", round(yahoo.price_cache[ticka],4)])
-                ascii_table.add_row(data.iloc[i])
+            ascii_table = danny_divito(data, ticka)
             await ctx.send(f"```\n{ascii_table.get_string()}\n```")
     except:
       await ctx.send("There was an error please try again")
@@ -65,6 +65,19 @@ async def safe_contracts(ctx, *args):
         data = yahoo.get_band(tokens[1])
         if tokens[0] == 'call' or tokens[0] == 'calls':
             data = data[['MA','STD','Upper']]
+            data = data.reset_index()
+            print(data)
+            calls = yahoo.return_calls(tokens[1])
+            calls = calls[['Contract Name', 'Strike']]
+            offset = len(tokens[1]) # for indexing contract names for date
+            calls['index'] = calls['Contract Name'].apply(lambda x: pd.to_datetime(x[4+offset:6+offset] + '-' + x[2+offset:4+offset] + '-' + x[offset:2+offset]))            
+            print(calls)
+            calls = calls.merge(data, how='left', on='index')
+            calls = calls.dropna()
+            calls['Safe'] = calls['Strike'] > calls['Upper']
+            ascii_table = danny_divito(calls, tokens[1])
+            await ctx.send(f"```\n{ascii_table.get_string()}\n```")
+            return
         elif tokens[0] == 'put' or tokens[0] == 'puts':
             data = data[['MA','STD','Lower']]
     
