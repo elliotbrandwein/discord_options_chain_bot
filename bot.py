a3917c388b6fddc1fd96a4cd30f7b79a7582b7c6
Jacob <jl@jacoblinder.net>
2020-12-06 22:43:55 -0500

    starting band command

--- bot.py
+++ bot.py
@@ -54,5 +54,22 @@ async def call_getter(ctx, ticka):
             await ctx.send(f"```\n{ascii_table.get_string()}\n```")
     except:
       await ctx.send("There was an error please try again")
+
+@bot.command(name='bands')
+async def safe_contracts(ctx, *args):
+    tokens = args[:]
+    if len(tokens) < 2:
+        await ctx.send("funtionality is >bands [call/put] [ticka]")
+        return
+    elif len(tokens) == 2:
+        data = yahoo.get_band(tokens[1])
+        if tokens[0] == 'call' or tokens[0] == 'calls':
+            data = data[['MA','STD','Upper']]
+        elif tokens[0] == 'put' or tokens[0] == 'puts':
+            data = data[['MA','STD','Lower']]
+    
+    
+
+    return
     
 bot.run(TOKEN)
